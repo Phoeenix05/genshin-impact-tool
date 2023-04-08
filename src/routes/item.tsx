48861e9d4e48bc689e408d58388bf5060d00c8d8
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-04-08 14:19:40 +0300

    added api routes

--- src/routes/item.tsx
+++ src/routes/item.tsx
@@ -1,5 +1,20 @@
+import { createResource, createSignal } from "solid-js"
+
+const fetchItemData = async () => {
+    return await fetch("/api/item", { 
+        method: "GET" 
+    }).then(res => res.text())
+}
+
 const ItemPage = () => {
-    return (<></>)
+    const [ itemName, setItemName ] = createSignal("hallo")
+    const [ itemData, {} ] = createResource(itemName, fetchItemData)
+    
+    return (
+        <pre>
+            { itemData() }
+        </pre>
+    )
 }
 
 export default ItemPage
